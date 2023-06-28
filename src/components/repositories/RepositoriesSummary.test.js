d117cc1adb5a51769d5eef0af353efa93f83ede8
Brigilets <b.sabutyte@gmail.com>
2023-06-28 17:32:18 +0200

    improved the test for language display to ensure all of the props are displayed

--- src/components/repositories/RepositoriesSummary.test.js
+++ src/components/repositories/RepositoriesSummary.test.js
@@ -1,16 +1,22 @@
 import { render, screen } from "@testing-library/react";
 import RepositoriesSummary from "./RepositoriesSummary";
 
-test("The component displays primary language of the repository", () => {
+test("Displays information about the repository", () => {
   const repository = {
     language: "Javascript",
     stargazers_count: 5,
-    forks: 2,
+    forks: 3,
     open_issues: 2,
   };
   render(<RepositoriesSummary repository={repository} />);
 
-  const language = screen.getByText("Javascript");
+  //   const language = screen.getByText("Javascript");
+  //   expect(language).toBeInTheDocument();
 
-  expect(language).toBeInTheDocument();
+  for (let key in repository) {
+    const value = repository[key];
+    const element = screen.getByText(new RegExp(value));
+
+    expect(element).toBeInTheDocument();
+  }
 });
